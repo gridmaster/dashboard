056b86e3bbd6cb5b30ebc54a11779e6af4512969
Jim Ballard <AntlerHorse@gmail.com>
2026-03-24 00:10:44 -0400

    registration page-fixed checkbox

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -3,68 +3,69 @@ import '../../assets/css/Register.css';
 
 const Register = () => {
     return (
+
         <div className='outerDivStyle'>
             <div >
-                <div className='welcomeDivStyle'>
-
-                        <table>
-                            <thead>
-                                <tr >
-                                    <th className='th-header-style' >Welcome to the Jungle</th>
-                                </tr>
-                                <tr >
-                                    <th className='p-style'>Please register to board the Nostromo</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr >
-                                    <th className='th-style'>Name</th>
-                                </tr>
-                                <tr >
-                                    <td>
-                                    <input className='tr-style tr-input-style' type='text' name='name' placeholder='Name' id='name' required />
-                                    </td>
-                                </tr>
-
-                                <tr >
-                                    <th className='th-style'>Email</th>
-                                </tr>
-                                <tr >
-                                    <th>
-                                        <input className='tr-style tr-input-style' type='text' name='email' placeholder='Email' id='email' required />
-                                    </th>
-                                </tr>
-
-                                <tr >
-                                    <th className='th-style'>Password</th>
-                                </tr>
-                                <tr >
-                                    <th>
-                                        <input className='tr-style tr-input-style' type='text' name='password' placeholder='Password' id='password' required />
-                                    </th>
-                                </tr>
-                            </tbody>
-                            <tfoot>
-                                <tr >
-                                    <th>
-                                        <div className='checkbox-container'>
-                                            <input className='div-input th-label th-input tr-checkbox-style' type='checkbox' name='checkbox' id='checkbox' />
-
-                                            <label className='th-label th-label-style'> I agree to privacy policy & terms</label>
-                                        </div>
-                                    </th>
-
-                                </tr>
-                            </tfoot>
-                        </table>
-
-                        <div className='tr-button-center'>
-                            <button className='tr-style tr-button-style'>Sign up!</button> 
-                        </div>
  
+                <div className='welcomeDivStyle'>
+                                   <form className='registration-form'>
+                    <table>
+                        <thead>
+                            <tr >
+                                <th className='th-header-style' >Welcome to the Jungle</th>
+                            </tr>
+                            <tr >
+                                <th className='p-style'>Please register to board the Nostromo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr >
+                                <th className='th-style'>Name</th>
+                            </tr>
+                            <tr >
+                                <td>
+                                <input className='tr-input-style tr-style' type='text' name='name' placeholder='Name' id='name' required />
+                                </td>
+                            </tr>
+
+                            <tr >
+                                <th className='th-style'>Email</th>
+                            </tr>
+                            <tr >
+                                <th>
+                                    <input className='tr-style tr-input-style' type='text' name='email' placeholder='Email' id='email' required />
+                                </th>
+                            </tr>
+
+                            <tr >
+                                <th className='th-style'>Password</th>
+                            </tr>
+                            <tr >
+                                <th>
+                                    <input className='tr-style tr-input-style' type='text' name='password' placeholder='Password' id='password' required />
+                                </th>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr >
+                                <th>
+                                    <div className='checkbox-container'>
+                                        <input className='tr-checkbox-style' type='checkbox' name='checkbox' id='checkbox' />
+
+                                        <label className='th-label th-label-style'> I agree to privacy policy & terms</label>
+                                        <button className='tr-style tr-button-style'>Sign up!</button>
+                                    </div>
+                                </th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                                     </form>
                 </div>
+
             </div>
+    
         </div>
+   
     );
 };
 
