42edcb7984035476cd47c40c2328f34d39a72fe4
Jim Ballard <AntlerHorse@gmail.com>
2026-04-07 01:15:50 -0400

    anchored the header need to fix when sidebar hid

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,23 +3,33 @@ import '../assets/css/Header.css';
 import { FaList } from "react-icons/fa";
 
 const Header = ({showSidebar, setShowSidebar}) => {
-    console.log("sb = " + showSidebar);
-    console.log(showSidebar === true);
-    console.log(showSidebar !== true);
-
-    //let span = document.getElementById("span-show-hide-text");
 
     return (
         
         <div className='header'>
-
-            <div id='header-div' className='responsive-div div-inner-header'>
+            <div className='div-inner-header left-anchored right-anchored'>
                 
-                <button onClick={() => setShowSidebar(!showSidebar)} className="btn"><FaList className='show-list-icon'/></button>
+                <button onClick={() => setShowSidebar(!showSidebar)} className="btn">
+                    <FaList className='show-list-icon'/>
+                </button>
 
                 <span className={`${showSidebar === true ? 'span-show-text' : 'span-hide-text'}`} > Hide sidebar</span>
                 <span className={`${showSidebar !== true ? 'span-show-text' : 'span-hide-text'}`} > Show sidebar</span>
 
+                <div className='hidden md:block'>
+                    <input className='header-text' type='text' />
+                </div>
+
+                <div className='admin-header'>
+                    <div className='place holder'>
+                        <div className='place holder'>
+                            <div className='div-user-info user-info flex justify-center items-center flex-col text-end'>
+                                <h5 className='h5-user-info'>Jim Ballard</h5>
+                                <span className='span-user-info'>Admin</span>
+                            </div>
+                        </div>
+                    </div >
+                </div>
             </div>
         </div>
     );
