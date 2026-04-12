e33050bb2cc051b6c23686a35b49372be0fd22a2
Jim Ballard <AntlerHorse@gmail.com>
2026-04-12 14:08:23 -0400

    commit before testing grid

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -5,8 +5,76 @@ import { RiCurrencyLine } from "react-icons/ri";
 import { MdProductionQuantityLimits } from "react-icons/md";
 import { IoIosPeople } from "react-icons/io";
 import { GiTakeMyMoney } from "react-icons/gi";
+import Chart from 'react-apexcharts';
 
 const AdminDashboard = () => {
+
+    const state = {
+        series: [
+            {
+                name: 'Orders',
+                data : [23,34,45,45,65,43,44,43,32,20,23,34]
+            },
+            {
+                name: 'Revenue',
+                data : [13,32,45,37,54,43,34,33,32,29,43,44]
+            },
+            {
+                name: 'Sellers',
+                data : [55,53,57,78,86,67,58,59,76,77,54,43]
+            }
+        ],
+        options : {
+            color : ['#181eee8', '#181eee8'],
+            plotOptions : {
+                radius : 30
+            },
+            chart : {
+                background : 'transparent',
+                foreColor : '#d0d2d6'
+            },
+            dataLabels : {
+                enabled : false
+            },
+            strock : {
+                show : true,
+                curve : ['smooth', 'straight', 'stepline'],
+                lineCap : 'butt',
+                colors : '#f0f0f0',
+                width : .5,
+                dashArray : 0
+            },
+            xaxis : {
+                categories : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            legend : {
+                position : 'top'
+            },
+            responsive : [
+                {
+                breakpoint : 565,
+                yaxis : {
+                    cateories : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                options : {
+                    plotOptions : {
+                        bar : {
+                            horizontal : true
+                        }
+                    },
+                    chart : {
+                        height : "550px"
+                    }
+                }
+            }
+            ]
+            
+        }
+
+    }
+
+
+
     return (
         <div className='dash-content-first'>
 
@@ -69,7 +137,7 @@ const AdminDashboard = () => {
             <div className='div-chart-outer'>
                 <div className='div-container'>
                     <div className='div-chart'>
-
+                        <Chart options={state.options} series={state.series} type='bar' height={350} />
                     </div>
                 </div>
             </div>
