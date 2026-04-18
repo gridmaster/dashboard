fe0f092efa621a5ff365b29e3bd4a7dca6a542b8
Jim Ballard <AntlerHorse@gmail.com>
2026-04-18 12:35:55 -0400

    pagination icons working - @Section 20

---
+++ src/views/Pagination.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { MdOutlineKeyboardDoubleArrowLeft, MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import '../assets/css/Pagination.css'
+
+
+    const Pagination = ({pageNumber, setPageNumber, totalItem, parPage, showItem}) => {
+
+        // pageNumber = {currentPage}
+        // setPageNumber = {setSearchValue}
+        // totalItem = {50}
+        // parPage = {parPage}
+        // showItem = {3}
+
+        let totalPage = Math.ceil(totalItem / parPage)
+        let startPage = pageNumber
+
+        let dif = totalPage - pageNumber
+        
+        if(dif <= showItem) {
+            startPage = totalPage - showItem
+        }
+
+        let endPage = startPage < 0 ? showItem : showItem + startPage
+
+        if( startPage <= 0) {
+            startPage = 1
+        }
+
+        console.log("pageNumber = " + pageNumber)
+        console.log("setPageNumber = " + setPageNumber)
+        console.log("totalItem = " + totalItem)
+        console.log("parPage = " + parPage)
+        console.log("showItem = " + showItem)
+        console.log("startPage = " + startPage)
+        console.log("endPage = " + endPage)
+        console.log("parPage = " + parPage)
+
+        const createBtn = () => {
+
+            const btns = []
+
+            for(let i = startPage; i<endPage; i++) {
+                btns.push(
+                    <li key={i} onClick={()=>setPageNumber(i)} className={` ${pageNumber === i ? '' : '' } ` }>
+                        {i}
+                    </li>
+                )
+                // console.log("i = " + i )
+                // console.log("pgnum = " + pageNumber)
+                // console.log(btns)
+            }
+            return btns
+        }
+
+        return (
+            <div className='page-nav-group'>
+                <ul className='page-ul-return page-pagination-btns'>
+                    <li className={0 < totalItem ? 'page-nav-btn' : 'page-nav-btn inactive'}><MdOutlineKeyboardDoubleArrowLeft /></li> 
+                    <li className={1 <= totalItem ? 'page-nav-btn' : 'page-nav-btn inactive'}>1</li> 
+                    <li className={2 <= totalItem ? 'page-nav-btn' : 'page-nav-btn inactive'}>2</li> 
+                    <li className={3 <= totalItem ? 'page-nav-btn' : 'page-nav-btn inactive'}>3</li>   
+                    <li className={4 <= totalItem ? 'page-nav-btn' : 'page-nav-btn inactive'}>4</li>
+                    <li className={5 <= totalItem ? 'page-nav-btn' : 'page-nav-btn inactive'}>5</li>
+                    <li className={6 <= totalItem ? 'page-nav-btn' : 'page-nav-btn inactive'}><MdOutlineKeyboardDoubleArrowRight /></li>
+                </ul>
+            </div>
+        )
+};
+
+export default Pagination;
--- src/views/admin/Orders.jsx
+++ src/views/admin/Orders.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 import '../../assets/css/Orders.css'
 import '../../assets/css/Admin.css';
 import '../../assets/css/Home.css';
+import Pagination from '../Pagination';
 
 const Orders = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [searchValue, setSearchValue] = useState('')
     const [parPage, setParPage] = useState(5)
-    
-    const state = true;
-
+    const [show, setShow] = useState(false)
+console.log("setCP = " + setCurrentPage)
     return (
         <div className='orders-div-content'>
 
@@ -43,14 +43,74 @@ const Orders = () => {
                     </div>
 
                     <div className='orders-div'>
-                        <div className='orders-detail-column'>#2343</div>
-                        <div className='orders-detail-column'>$45</div>
+                        <div className='orders-detail-column'>#34534</div>
+                        <div className='orders-detail-column'>$345</div>
                         <div className='orders-detail-column'>Received</div>
                         <div className='orders-detail-column'>Pending</div>
                         <div className='orders-detail-column'>Y</div>
                         <div className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
                     </div>
 
+                    <div className='orders-div'>
+                        <div className='orders-detail-column'>#34534</div>
+                        <div className='orders-detail-column'>$345</div>
+                        <div className='orders-detail-column'>Received</div>
+                        <div className='orders-detail-column'>Pending</div>
+                        <div className='orders-detail-column'>Y</div>
+                        <div className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
+                    </div>
+
+                    <div className='orders-div'>
+                        <div className='orders-detail-column'>#34534</div>
+                        <div className='orders-detail-column'>$345</div>
+                        <div className='orders-detail-column'>Received</div>
+                        <div className='orders-detail-column'>Pending</div>
+                        <div className='orders-detail-column'>Y</div>
+                        <div className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
+                    </div>
+
+                    <div className='orders-div'>
+                        <div className='orders-detail-column'>#34534</div>
+                        <div className='orders-detail-column'>$345</div>
+                        <div className='orders-detail-column'>Received</div>
+                        <div className='orders-detail-column'>Pending</div>
+                        <div className='orders-detail-column'>Y</div>
+                        <div className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
+                    </div>
+
+                    <div className='orders-div'>
+                        <div className='orders-detail-column'>#34534</div>
+                        <div className='orders-detail-column'>$345</div>
+                        <div className='orders-detail-column'>Received</div>
+                        <div className='orders-detail-column'>Pending</div>
+                        <div className='orders-detail-column'>Y</div>
+                        <div className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
+                    </div>
+                    
+                    <div className='orders-div'>
+                        <div className='orders-detail-column'>#2343</div>
+                        <div className='orders-detail-column'>$45</div>
+                        <div className='orders-detail-column'>Received</div>
+                        <div className='orders-detail-column'>Pending</div>
+                        <div className='orders-detail-column'>Y</div>
+                        <div onClick={(e) => setShow(!show)} className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
+                    </div>
+
+                    <div className={ show ? 'orders-order-detail' : 'hidden' }>
+                        <div className='orders-div-detail'>
+                            <div className='orders-detail-column text-color'>#9032</div>
+                            <div className='orders-detail-column text-color'>$22</div>
+                            <div className='orders-detail-column text-color'>Received</div>
+                            <div className='orders-detail-column text-color'>Pending</div>
+                        </div>
+
+                        <div className='orders-div-detail'>
+                            <div className='orders-detail-column text-color'>#9032</div>
+                            <div className='orders-detail-column text-color'>$22</div>
+                            <div className='orders-detail-column text-color'>Received</div>
+                            <div className='orders-detail-column text-color'>Pending</div>
+                        </div>
+                    </div>
 
                     <div className='orders-div'>
                         <div className='orders-detail-column'>#9032</div>
@@ -58,16 +118,15 @@ const Orders = () => {
                         <div className='orders-detail-column'>Received</div>
                         <div className='orders-detail-column'>Pending</div>
                         <div className='orders-detail-column'>Y</div>
-                        <div className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
+                        <div onClick={(e) => setShow(!show)} className='orders-detail-column'><FaArrowsDownToLine className='on-hover-row' /></div>
                     </div>
             
-                    <div className={ state ? 'orders-order-detail' : 'hidden' }>
+                    <div className={ show ? 'orders-order-detail' : 'hidden' }>
                         <div className='orders-div-detail'>
                             <div className='orders-detail-column text-color'>#9032</div>
                             <div className='orders-detail-column text-color'>$22</div>
                             <div className='orders-detail-column text-color'>Received</div>
                             <div className='orders-detail-column text-color'>Pending</div>
-
                         </div>
 
                         <div className='orders-div-detail'>
@@ -75,9 +134,16 @@ const Orders = () => {
                             <div className='orders-detail-column text-color'>$22</div>
                             <div className='orders-detail-column text-color'>Received</div>
                             <div className='orders-detail-column text-color'>Pending</div>
-
                         </div>
                     </div>
+
+                    <Pagination 
+                        pageNumber = {currentPage}
+                        setPageNumber = {setSearchValue}
+                        totalItem = {3}
+                        parPage = {parPage}
+                        showItem = {5}
+                    />
                 </div> 
             </div>
         </div>
